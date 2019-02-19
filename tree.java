ad1666f1dce9944064437ab59d217daab1c9fd52
mateus-dev <43862225+mateus-dev@users.noreply.github.com>
2019-02-19 16:47:07 -0300

    Code that prints a christmas tree
    Learning the basics of System.out.println and printf in Java, this code will
    print a christmas tree on the terminal.

---
+++ tree.java
@@ -0,0 +1,16 @@
+public class tree {
+	public static void main (String[]args) 	{
+		// println
+		System.out.println ("  *  ");
+		System.out.println (" *** ");
+		System.out.println ("*****");
+		System.out.println (" _|_ ");
+
+		System.out.println();
+		// printf
+		System.out.printf ("  *  \n"+
+		                   " *** \n"+
+		                   "*****\n"+
+		                   " _|_ \n");
+	}
+}
